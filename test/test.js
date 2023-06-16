05ba513d3ee5425541577c75f4c89ab821a24dd2
Matthew Duffy <mattduffy@gmail.com>
2023-06-15 21:51:32 -0700

    Nothing

--- test/test.js
+++ test/test.js
@@ -1,18 +1,19 @@
 import { promisify } from 'node:util'
+import { resolve } from 'node:path'
 import { exec } from 'node:child_process'
 import { Album } from '../src/index.js'
 import { _log, _error } from '../src/utils/debug.js'
 
 const exiftool = promisify(exec)
 
-// console.log(`pwd: ${process.cwd()}`)
-const cwd = process.cwd()
+const cwd = resolve(process.cwd())
 const jpg = `${cwd}/test/IMG_1820.jpg`
 const jpg1 = `${cwd}/test/copper.jpg`
 const heic = `${cwd}/test/IMG_1820.heic`
 const name = process.env.SITE_NAME ?? 'no name'
 const log = _log.extend('test')
 const error = _error.extend('test')
+log(`pwd: ${cwd}`)
 log(`${name}`)
 
 async function getAllExifData(img) {
@@ -21,25 +22,27 @@ async function getAllExifData(img) {
   error(stderr)
 }
 
+// getAllExifData(heic)
+// getAllExifData(jpg)
+
 /*
  * -s1    print tag names instead of description
  * -s2    no extra spaces to column-align values
  * -s3    print values only
  * -json  output in json format
  */
-const opts = `-config ${cwd}/exiftool.config -json -s3 -G -c "%.6f"`
+const opts = `-config ${resolve(cwd, './config')}/exiftool.config -json -s3 -G -c "%.6f"`
 async function getExifTagValue(options, tag, img) {
-  const { stdout, stderr } = await exiftool(`/usr/local/bin/exiftool ${options} ${tag} ${img}`)
-  log(stdout)
-  error(stderr)
+  const _cmd = `/usr/local/bin/exiftool ${options} ${tag} ${img}`
+  log(`_cmd: ${_cmd}`)
+  const { stdout, stderr } = await exiftool(_cmd)
+  log(JSON.parse(stdout))
+  // error(stderr)
 }
 
-// getAllExifData(heic)
-// getAllExifData(jpg)
-
-getExifTagValue(opts, '-ObjectName -MattsShortcut', jpg1)
-// getExifTagValue(opts, '-ObjectName -Keywords', jpg1)
-// getExifTagValue(opts, '-Keywords', jpg1)
+getExifTagValue(opts, '-ObjectName -MattsShortcut', jpg)
+getExifTagValue(opts, '-ObjectName -MyShortcut', jpg1)
+getExifTagValue(opts, '-Keywords', heic)
 
 export {
   getExifTagValue as getTag,
