4a9c78c377edb3300c4ef31e1b565e8ff34ee676
Matthew Duffy <mattduffy@gmail.com>
2023-06-16 21:55:02 -0700

    Add constructor method to Album class and associated unit test methods.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 /**
  * @module @mattduffy/albums
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ * @file src/index.js The Album class definition file.
  */
 
+import path from 'node:path'
+import fs from 'node:fs/promises'
 import { _log, _error } from './utils/debug.js'
 
 /**
- * @todo [ ] do stuff
- *
- */
-
-/**
- * A class...
- * @summary A class...
- * @author mattduffy@gmail.com
- * @module @mattduffy/albums
+ * A class to model the shape a properties of an album of items, usually photos.
+ * @summary A class to model the shape a properties of an album of items, usually photos.
+ * @class Album
+ * @author Matthew Duffy <mattduffy@gmail.com>
  */
 class Album {
+  #rootDir
+
+  /**
+   * Create an instance of Album.
+   * @summary Create an instance of Album.
+   * @param { Object } config - An object literal contain configuration properties.
+   * @return { Album }
+   */
+  constructor(config = {}) {
+    const log = _log.extend('constructor')
+    const error = _error.extend('constructor')
+    this.#rootDir = config?.rootDir ?? process.env.ALBUMS_ROOT_DIR ?? './albums'
+  }
+
+  get rootDir() {
+    return this.#rootDir
+  }
+
+  set rootDir(dirPath) {
+    this.#rootDir = dirPath
+  }
 
 }
-_log('hellow')
-_error('ouch')
+
 export { Album }
--- test/test.js
+++ test/test.js
@@ -1,50 +1,26 @@
-import { promisify } from 'node:util'
-import { resolve } from 'node:path'
+import { describe, it } from 'node:test'
 import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
+import assert from 'node:assert/strict'
+import path from 'node:path'
+import fs from 'node:fs/promises'
 import { Album } from '../src/index.js'
 import { _log, _error } from '../src/utils/debug.js'
 
-const exiftool = promisify(exec)
-
-const cwd = resolve(process.cwd())
-const jpg = `${cwd}/test/IMG_1820.jpg`
-const jpg1 = `${cwd}/test/copper.jpg`
-const heic = `${cwd}/test/IMG_1820.heic`
-const name = process.env.SITE_NAME ?? 'no name'
 const log = _log.extend('test')
 const error = _error.extend('test')
-log(`pwd: ${cwd}`)
-log(`${name}`)
 
-async function getAllExifData(img) {
-  const { stdout, stderr } = await exiftool(`/usr/local/bin/exiftool -list ${img}`)
-  log(stdout)
-  error(stderr)
-}
-
-// getAllExifData(heic)
-// getAllExifData(jpg)
-
-/*
- * -s1    print tag names instead of description
- * -s2    no extra spaces to column-align values
- * -s3    print values only
- * -json  output in json format
- */
-const opts = `-config ${resolve(cwd, './config')}/exiftool.config -json -s3 -G -c "%.6f"`
-async function getExifTagValue(options, tag, img) {
-  const _cmd = `/usr/local/bin/exiftool ${options} ${tag} ${img}`
-  log(`_cmd: ${_cmd}`)
-  const { stdout, stderr } = await exiftool(_cmd)
-  log(JSON.parse(stdout))
-  // error(stderr)
-}
-
-getExifTagValue(opts, '-ObjectName -MattsShortcut', jpg)
-getExifTagValue(opts, '-ObjectName -MyShortcut', jpg1)
-getExifTagValue(opts, '-Keywords', heic)
-
-export {
-  getExifTagValue as getTag,
-  getAllExifData as getAllTags,
-}
+describe('First test for albums package', async () => {
+  const album = new Album()
+  it('should import and instantiate the Album class', () => {
+    assert(album instanceof Album)
+  })
+  it('should have a rootDir path assigned', async () => {
+    assert.notStrictEqual(album.rootDir, undefined)
+    log(path.resolve(album.rootDir))
+  })
+  it('should have a rootDir that actually exists', async () => {
+    const stats = await fs.stat(path.resolve(album.rootDir))
+    log(`stats.isDirectory: ${stats.isDirectory()}`)
+  })
+})
