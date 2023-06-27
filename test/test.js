59b72c09da21c45c803c323ad03b87e190e41937
Matthew Duffy <mattduffy@gmail.com>
2023-06-26 21:19:26 -0700

    Resolved async/await issue preventing tests from completing.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
-import { describe, it, after } from 'node:test'
+import {
+  after,
+  before,
+  describe,
+  it,
+} from 'node:test'
 import assert from 'node:assert/strict'
 import path from 'node:path'
 import fs from 'node:fs/promises'
+import { Unpacker } from '@mattduffy/unpacker' // eslint-disable-line import/no-unresolved
 import { Album } from '../src/index.js'
 import { io as redis } from '../lib/redis-client.js'
-import { client as mongo } from '../lib/mongodb-client.js'
+// import { client as mongo } from '../lib/mongodb-client.js'
 import {
   _log,
   _info,
@@ -14,28 +20,46 @@ import {
 
 const log = _log.extend('test')
 const info = _info.extend('test')
-const warn = _warn.extend('test')
-const error = _error.extend('test')
+const warn = _warn.extend('test') // eslint-disable-line no-unused-vars
+const error = _error.extend('test') // eslint-disable-line no-unused-vars
+const rootDir = path.resolve('tmp/albums')
+info(rootDir)
+const uploads = process.env.uploadsDir ?? 'tmp/uploads'
+const archive = `${uploads}/marquetry.tgz`
+info(archive)
 
 describe('First test for albums package', async () => {
-  after(() => {
+  before(async () => {
+  })
+
+  after(async () => {
     redis.quit()
-    mongo.quit()
+    // mongo.quit()
   })
   const opts = {
+    // mongo,
     redis,
-    mongo,
+    rootDir,
   }
-  const album = new Album(opts)
+  let album = new Album(opts)
   it('should import and instantiate the Album class', () => {
     assert(album instanceof Album)
   })
+
   it('should have a rootDir path assigned', async () => {
     assert.notStrictEqual(album.rootDir, undefined)
     log(path.resolve(album.rootDir))
   })
+
   it('should have a rootDir that actually exists', async () => {
+    album = await album.init()
     const stats = await fs.stat(path.resolve(album.rootDir))
     log(`stats.isDirectory: ${stats.isDirectory()}`)
+    assert.strictEqual(rootDir, album.rootDir)
+  })
+
+  it('should ...', async () => {
+    const unpacker = new Unpacker()
+    await unpacker.setPath(archive)
   })
 })
